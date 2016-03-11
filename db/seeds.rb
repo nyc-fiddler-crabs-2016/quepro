93b8166e049494eaec36d56e379e22b49762c693
Joe Case <jcase146@gmail.com>
2016-03-11 14:39:59 -0500

    Add to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 user_amount           = 6
 survey_amount         = 10
 questions_per_survey  = 5
+
+surveys_taken_amount  = 20
 choices_per_question  = 4
 responses_per_choice  = 2
 
@@ -17,11 +19,10 @@ end
 user_ids = User.all.map { |user| user.id }
 
 
-
 # Surveys
 survey_amount.times do
   survey = Survey.create(title:     Faker::Hipster.sentence(2),
-                          user_id:   user_ids.sample)
+                         user_id:   user_ids.sample)
 
    questions_per_survey.times do
     question = Question.create(name:       Faker::Hipster.sentence(3),
@@ -38,4 +39,12 @@ survey_amount.times do
 end
 
 survey_ids = Survey.all.map { |survey| survey.id }
+
+surveys_taken_amount.times do
+  SurveysTaken.create(user_id: user_ids.sample,
+                      survey_id: survey_ids.sample)
+end
+
+
+
 
